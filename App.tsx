c99262b08e2bb82d8936f4ab52b02aa7b42f7f36
Thais Cristine <this.cristine06@gmail.com>
2024-02-16 13:01:43 +0100

    Custom fonts to the application

--- App.tsx
+++ App.tsx
@@ -1,11 +1,18 @@
 import { Groups } from '@screens/Groups';
 import { ThemeProvider } from 'styled-components';
 import theme from './src/theme';
+import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
+import { ActivityIndicator } from 'react-native';
 
 export default function App() {
+
+  const [fontLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold
+  });
   return (
     <ThemeProvider theme={theme}>
-      <Groups />
+      {fontLoaded ? <Groups /> : <ActivityIndicator /> }
     </ThemeProvider>
   );
 }
