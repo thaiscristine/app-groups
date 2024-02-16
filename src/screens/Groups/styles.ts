d950fcad0c7732519572e2f4e3e8f1683f5fdadc
Thais Cristine <this.cristine06@gmail.com>
2024-02-16 11:14:45 +0100

    Styled components

---
+++ src/screens/Groups/styles.ts
@@ -0,0 +1,13 @@
+import styled from "styled-components/native";
+
+export const Container = styled.View`
+  flex: 1;
+  background-color: black;
+  align-items: center;
+  justify-content: center;
+`;
+export const Title = styled.Text`
+  color: white;
+  font-size: 24px;
+  font-weight: 100;
+`;
